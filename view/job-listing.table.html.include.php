f53647c39f3db8abae846f6f702aad566dbf2411
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2016-10-21 19:15:11 -0700

    Started framing the form
    This really just has the outline of the form, nothing really in it that can be said to be anywhere near an actual form.  Need to look closer at what I need to do for forms to get this to work properly.
    
    I seem to have forgotten half of what I used to know about these things.  Hopefully all of it falls into place with a little bit of looking.

---
+++ view/job-listing.table.html.include.php
@@ -0,0 +1,26 @@
+<table>
+	Job Title: 
+	Job Type:
+		For Autistics:
+		Helping Autistics:
+		Other:
+			Other Description:
+	Job Location:
+		Steet Address:
+		City:
+		Region:
+		Province/State:
+		Country:
+	Job Description:
+	Job Requirements:
+	Ideal Candidate:
+	How to Apply:
+		Postal Address:
+		In Person:
+		Email:
+		Website:
+	Company:
+		Name:
+		Website:
+</table>
+
