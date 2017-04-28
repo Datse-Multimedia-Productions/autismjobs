8aa6ded19188220efe85c74b3e873ea02e14aa16
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-04-27 22:38:27 -0700

    Added index call to actions
    Added the call to the index action called "home page" in the actions.

--- view/actions.html.include.php
+++ view/actions.html.include.php
@@ -3,4 +3,5 @@
 <ul>
         <li><a href="/index.php?action=addListing">Add A Job Listing</a></li>
         <li><a href="/index.php?action=register">Register</a></li>
+	<li><a href="/index.php?action=index">Homepage</a></li>
 </ul>
