64dbdc0829e856026b7bb95c9cd9e22087ccab67
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-04-27 21:22:14 -0700

    Added Profile Information
    Basic profile information has been added.  Currently we are not checking
    to see if the primary email is unique in the table.

--- model/creatdbschma.sql
+++ model/creatdbschma.sql
@@ -1,8 +1,15 @@
 CREATE TABLE users (
 	userid		SERIAL,
 	username	VARCHAR(80),
-	salt		CHAR(6),
 	password	CHAR(32),
 	created		TIMESTAMP,
 	lastlogin	TIMESTAMP
 )
+
+CREATE TABLE profiles (
+	profileid	SERIAL,
+	userid		INTEGER NOT NULL,
+	displayname	VARCHAR(80),
+	primaryemail	VARCHAR(255)
+}
+
