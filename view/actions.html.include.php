384a66f3b93c9b3dfe52a8e2ca397c9690506327
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-04-27 22:01:21 -0700

    move actions to separate file
    Move the actions file to a separate file to make things easier to
    maintain, so when action list changes, only one file needs to be
    updated.
    
    This may need some rethinking down the road, as we could be just listing
    the actions as a list, or something like that, but this will do for now.

---
+++ view/actions.html.include.php
@@ -0,0 +1,6 @@
+<h2>Actions</h2>
+
+<ul>
+        <li><a href="/index.php?action=addListing">Add A Job Listing</a></li>
+        <li><a href="/index.php?action=register">Register</a></li>
+</ul>
--- view/index.html.php
+++ view/index.html.php
@@ -7,12 +7,9 @@ require_once("header.html.php");
 ?>
 <p>We were reading (more scanning) on Hacker News about someone who created a website for older people in the tech industry, so decided to give it a try to do similar for Autistic people, and companies supporting Autistics</p>
 
-<h2>Actions</h2>
-
-<ul>
-	<li><a href="/index.php?action=addListing">Add A Job Listing</a></li>
-	<li><a href="/index.php?action=register">Register</a></li>
-</ul>
+<?php
+require("actions.html.php");
+?>
 
 <h2>Job Listings</h2>
 	<p>Here are your job listings</p>
