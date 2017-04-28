3a91a48b9ef4fdb724515d591d1c330795dc2eff
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-04-27 21:52:28 -0700

    Added actions to addlisting and register
    Added links to add listing and to register.  These aren't very well
    implemented yet, or maybe even at all.  But we are getting closer.

--- controller/index.php
+++ controller/index.php
@@ -6,6 +6,12 @@ switch ($action) {
 	case "index":
 		require_once("view/index.html.php");
 		break;
+	case "addListing":
+		require_once("view/addlisting.html.php");
+		break;
+	case "register":
+		require_once("view/register.html.php");
+		break;
 	default:
 		require_once("view/index.html.php");
 }
