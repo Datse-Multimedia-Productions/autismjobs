9c7c847079b82d5552079c2349e81234e7469e2c
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-04-27 22:29:38 -0700

    Insert <br /> to break lines
    Did not have <br /> breaking lines.  It looks like it displays better
    now.

--- view/register.form.html.include.php
+++ view/register.form.html.include.php
@@ -1,12 +1,12 @@
 <form action="/index.php" method="post">
 	<label for="userame">Username</label>:
-	<input type="text" name="username" id="username">
+	<input type="text" name="username" id="username"><br />
 	<label for="email">Email</label>:
-	<input type="text" name="email" id="email">
+	<input type="text" name="email" id="email"><br />
 	<label for="password">Password</label>:
-	<input type="password" name="password" id="password">
+	<input type="password" name="password" id="password"><br />
 	<label for="passwordConfirm">Confirm Password</label>:
-	<input type="password" name="passwordConfirm" id="passwordConfirm">
+	<input type="password" name="passwordConfirm" id="passwordConfirm"><br />
 	<input type="submit" value="preview"><input type="reset">
 </table> 
 
