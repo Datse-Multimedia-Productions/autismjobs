4d411d492e622b425ab3db082e5b9b1d0b7cdcda
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-04-27 21:45:10 -0700

    Create registration form
    Create form for registration.  There likely will be changes to this
    fairly quickly.

---
+++ view/register.form.html.include.php
@@ -0,0 +1,13 @@
+<form action="/index.php" method="post">
+	<label for="userame">Username</label>:
+	<input type="text" name="username" id="username">
+	<label for="email">Email</label>:
+	<input type="text" name="email" id="email">
+	<label for="password">Password</label>:
+	<input type="password" name="password" id="password">
+	<label for="passwordConfirm">Confirm Password</label>:
+	<input type="password" name="passwordConfirm" id="passwordConfirm">
+	<input type="submit" value="preview"><input type="reset">
+</table> 
+
+
