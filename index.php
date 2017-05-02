b78881a50fc708e1a8397ba8c20b185f0df26fca
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-01 20:48:42 -0700

    Remove old code...
    Cleaning up commented out code, and the `config.data.php` file which has
    been replaced.

--- index.php
+++ index.php
@@ -8,13 +8,9 @@ if (!file_exists("$configFileName")) {
 	echo "This is not configured";
 	echo "please configure this.";
 } else {
-	// configured, this should call the controller
-	// load configuration
-	// require_once ("config.data.php"); // Load Configuration Data
-	// print_r($config); // This is for diagnosis.  Should be in a "if debug".
-	// configuration loaded...  Check validity.
+	// load and check configuration. 
 	require_once ("model/check-config.php"); // Load config check routines. 
-	// checkAll($config); // Call to check all elements in configuration.
+	
 	// call controller.
 	require_once ("controller/index.php");
 }
