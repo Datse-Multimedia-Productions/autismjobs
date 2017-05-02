8254783b7331dd5f5ff0c8c05607b21de5880b1d
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-01 19:27:25 -0700

    Starting conversion to YAML config file
    I coppied initial configuration to a YAML file (hopefully I did it
    right) and edited the old configuration load script to load it and parse
    it.  This should have the identical result as the old version.

--- config.data.php
+++ config.data.php
@@ -1,16 +1,22 @@
 <?php 
 
-$config = array (
-	"database" => array (
-		"type" => "psql",
-		"host" => "localhost",
-		"port" => "5432",
-		"dbname" => "pleaseset",
-		"user" => "setuser",
-		"password" => "setpassword"
-	),
-	"configured" => TRUE
-);
+// Configuration File Name
+$configFileName="config.yaml";
+
+// Load yaml file into variable
+$config=file_get_contents($configFileName);
+
+//$config = array (
+//	"database" => array (
+//		"type" => "psql",
+//		"host" => "localhost",
+//		"port" => "5432",
+//		"dbname" => "pleaseset",
+//		"user" => "setuser",
+//		"password" => "setpassword"
+//	),
+//	"configured" => TRUE
+//);
 
 // print_r($config); // this is for diagnostic purposes...  Should be removed.
 
