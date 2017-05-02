65968da3e56fee7e455148243c39e79251a507ae
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-02 15:24:18 -0700

    Copied includes from main to admin
    Copied the common includes from the main site, to the admin includes
    directory.  Only copied the default ones.
    
    I want to be able to have something like a `template_include()` function
    which will pull in the most "specific version" of that file.
    
    That will be down the road.

---
+++ view/admin/includes/actions.html.include.php
@@ -0,0 +1,7 @@
+<h2>Actions</h2>
+
+<ul>
+        <li><a href="/index.php?action=addListing">Add A Job Listing</a></li>
+        <li><a href="/index.php?action=register">Register</a></li>
+	<li><a href="/index.php?action=index">Homepage</a></li>
+</ul>
