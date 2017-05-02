1ece8aa1017bec238bae68e00ed7535716afc984
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-02 11:52:32 -0700

    Bit of cleanup on admin
    This is cleaning up a few things with the admin...  Removing the
    temporary view, and getting the controller to call the view in the admin
    view directory.

--- controller/admin.php
+++ controller/admin.php
@@ -1,3 +1,14 @@
-<?php
+<?php 
+
+require_once("model/servervars.php");
+require_once("model/database.php");
+
+switch ($action) {
+	case "index":
+		require_once("view/admin/index.html.php");
+		break;
+	default:
+		require_once("view/admin/index.html.php");
+}
 
 ?>
