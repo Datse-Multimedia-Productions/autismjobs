5f153e7def5c5316c49868d05f466e3b10d1ae6a
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-01 20:15:15 -0700

    This is now working
    I seem to have this working.  The YAML file wasn't quite formatted
    correctly (it doesn't like tabs or commas), and I have cleaned up the
    loading file so it is a lot slimmer.
    
    I want to make sure that I can move both of these files to somewhere
    that I can ensure that they can't be directly acessed by users browsing
    through my directories (once I have security setup).

--- config.data.php
+++ config.data.php
@@ -8,18 +8,6 @@ $configYAML=file_get_contents($configFileName);
 
 $config=yaml_parse($configYAML);
 
-//$config = array (
-//	"database" => array (
-//		"type" => "psql",
-//		"host" => "localhost",
-//		"port" => "5432",
-//		"dbname" => "pleaseset",
-//		"user" => "setuser",
-//		"password" => "setpassword"
-//	),
-//	"configured" => TRUE
-//);
-
 // print_r($config); // this is for diagnostic purposes...  Should be removed.
 
 ?>
