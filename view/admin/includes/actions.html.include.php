49522c617c050e7dff06da9ab169b2ef821fb7f5
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-02 15:32:28 -0700

    Start of getting admin section together
    This is the start of being able to create the admin section in a
    reasonable way.

--- view/admin/includes/actions.html.include.php
+++ view/admin/includes/actions.html.include.php
@@ -1,7 +1,6 @@
 <h2>Actions</h2>
 
 <ul>
-        <li><a href="/index.php?action=addListing">Add A Job Listing</a></li>
-        <li><a href="/index.php?action=register">Register</a></li>
 	<li><a href="/index.php?action=index">Homepage</a></li>
+	<li><a href="/admin.php?action=index">Admin Page</a></li>
 </ul>
