774caa7b352bd87c681cce16268e7b85780ce613
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-02 16:19:06 -0700

    Start of Create Database stuff
    This will create the tables.  Right now, we are *expecting* that we will
    not try to create role and database itself, but that will have to be
    manually handled.

--- model/database.php
+++ model/database.php
@@ -1,26 +1,19 @@
 <?php
 
-// $dsn=$config["database"]["type"].
-// ":host=".$config["database"]["host"].
-// ";port=".$config["database"]["port"].
-// ";dbname=".$config["database"]["dbname"].
-// ";user=".$config["database"]["user"].
-// ";password=".$config["database"]["password"];
-
 $pgconnection="host=".$config['database']['host'].
 " dbname=".$config['database']['dbname'].
 " user=".$config['database']['user'].
 " password=".$config['database']['password'];
 
-// echo $pgconnection;
-
-// echo $dsn;
-
-//$dbh=new PDO($dsn);
-
-//$dbh=new PDO("mysql:host=localhost;port=5432;dbname=pleaseset;user=setuser;password=setpassword");
-
 $connection=pg_connect($pgconnection);
+
+// This will create the database tables
+function createDatabaseTables($connection, $sqlfile) {
+	$sqlcommand = file_get_contents($sqlfile);
+	if ($sqlcommand) {
+		pg_query($connection, $sqlcommand);
+	}
+}
 
 ?>
 
