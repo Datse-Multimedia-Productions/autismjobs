7cf0e32af2e6a36bf71ea05098e0f58797769ea8
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 15:53:28 -0700

    Added default for create and removed profile
    I have added a default for the create time, and I removed the profile
    table which isn't (or shouldn't need to be) needed for
    login/registration.
    
    Profiles will be used more closely with job listings, and eventually job
    applications.

--- model/users.sql
+++ model/users.sql
@@ -3,16 +3,9 @@ CREATE EXTENSION pgcrypto;
 CREATE TABLE users (
 	userid		uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	username	text NOT NULL,
-	email		text NOT NULL
+	email		text NOT NULL,
 	password	text  NOT NULL,
-	created		TIMESTAMP NOT NULL,
+	created		TIMESTAMP NOT NULL DEFAULT now(),
 	lastlogin	TIMESTAMP
 )
-
-CREATE TABLE profiles (
-	profileid	INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE,
-	userid		INTEGER NOT NULL REFERENCES users (userid),
-	displayname	VARCHAR(80),
-	primaryemail	VARCHAR(255) NOT NULL
-}
 
