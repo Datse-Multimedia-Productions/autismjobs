4ec3030e2688d96e5763e482d8f68e1fcb774dda
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 19:56:22 -0700

    Call to create users table
    This should create the users table.  I know a lot of this needs to be
    cleaned up, but let's give this a try right now.

--- model/createdatabase.php
+++ model/createdatabase.php
@@ -2,5 +2,14 @@
 
 $tables=cleanPostArray("tables");
 
+foreach ($tables as $table) {
+	switch ($table) {
+		case "users":
+			createDatabaseTables($connection, "users.sql");
+			break;
+		default:
+			echo "No such tables";
+	}
+}
 
 ?>
