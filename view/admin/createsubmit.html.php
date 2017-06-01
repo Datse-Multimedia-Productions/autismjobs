fce232b4103a86476b45401a1cada391f0723c9c
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 19:50:00 -0700

    Start of display of results
    This will display results, but currently we need to figure that out.

---
+++ view/admin/createsubmit.html.php
@@ -0,0 +1,22 @@
+<?php 
+
+$title="Autism Jobs - Admin - Create Database Results";
+$pageHead="Autism Jobs Admin Page - Create Database Results";
+
+require_once("includes/header.html.php");
+?>
+<p>This is the admin section of the website.  I think we need to do a few different things here</p>
+
+<h2>Create Database Results</h2>
+
+<p>These are the results of the database creation that you just did:</p>
+
+<?php 
+echo "I need to figure out how I am handling this";
+?>
+
+<?php
+require_once("includes/actions.html.include.php");
+?>
+
+<?php require_once("includes/footer.html.php"); ?>
