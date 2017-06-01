a5d2db6592b5d21261205d5a048699b249004d71
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 19:21:50 -0700

    Use array for tables and formatting
    This is now using an array for tables, and has changed a minor
    formatting thing, putting existing table `users` on own line with a `<br
    />`.

--- view/admin/includes/create-form.include.html.php
+++ view/admin/includes/create-form.include.html.php
@@ -1,5 +1,5 @@
 <form action="/admin.php?action=createsubmit" method="post">
-    <input type="checkbox" name="users" id="users" value="users">
-    <label for="users">User Database</label>
+    <input type="checkbox" name="tables[]" id="users" value="users">
+    <label for="users">User Database</label><br />
     <input type="submit" value="Submit">
 </form>
