2f4527dfe5e524884ca4f8df0125472491c7619c
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 18:24:48 -0700

    Form to create user database
    This form once I create links to calling, will allow a person to create
    the user table in the database.
    
    I will need to create a way to verify in the future.

---
+++ view/admin/includes/create-form.include.html.php
@@ -0,0 +1,5 @@
+<form action="/admin.php?action=createsubmit" method="post">
+    <input type="checkbox" name="users" id="users" value="users">
+    <label for="users">User Database</label>
+    <input type="submit" value="Submit">
+</form>
