691a7ff1a5972626924151f40a1348a59f453faa
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 18:32:14 -0700

    Added call to handle submission of databasecreate
    This will call the database creation routines, and display the results.

--- controller/admin.php
+++ controller/admin.php
@@ -10,6 +10,10 @@ switch ($action) {
 	case "create":
 		require_once("view/admin/create.html.php");
 		break;
+	case "createsubmit"
+		require_once("model/createdatabase.php");
+		require_once("view/admin/createsubmit.html.php");
+		break;
 	default:
 		require_once("view/admin/index.html.php");
 }
