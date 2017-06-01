b0cd2253555f6cdd597b6b2b992f02752e00adac
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 20:15:13 -0700

    Start of trying to handle how to express this.
    This is the start of handling how to put the output.  I think I am
    missing some stuff here.

--- model/createdatabase.php
+++ model/createdatabase.php
@@ -6,6 +6,7 @@ foreach ($tables as $table) {
 	switch ($table) {
 		case "users":
 			createDatabaseTables($connection, "model/users.sql");
+			$output[$table]="Table $table created";
 			break;
 		default:
 			echo "No such tables";
