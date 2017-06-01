f94c14434a693463806844cb96da6e06830ef481
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 21:29:12 -0700

    Added documentation
    Added file level docblock.  This has the default tags that I think I
    want in pretty much every file level docblock.  Currently I think that I
    will keep this format also for other level docblocks.

--- model/createdatabase.php
+++ model/createdatabase.php
@@ -1,6 +1,25 @@
 <?php
+/**
+ * Create the database tables 
+ *
+ * This calls what is needed to create the database tables.
+ * 
+ *
+ * @copyright 2016-2017 Datse Multimedia Productions
+ * @license GPL
+ * @license https://opensource.org/licenses/GPL-3.0 GNU Public License, version 3
+ * @author Jigme Datse Yli-Rasku <jigme.datse@datemultimedia.com>
+ * 
+ * @todo We need to look at profiles and job listings still
+ *
+ * @since 0.0.0 
+ * @version 0.0.0 
+ */
 
 $tables=cleanPostArray("tables");
+
+
+
 
 foreach ($tables as $table) {
 	switch ($table) {
@@ -9,7 +28,7 @@ foreach ($tables as $table) {
 			$output[$table]="Table $table created";
 			break;
 		default:
-			echo "No such tables";
+			$output[$table]="No such table $table";
 	}
 }
 
