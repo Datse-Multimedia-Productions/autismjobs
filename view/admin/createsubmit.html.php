ae9613951fdd61ecda99f7a4fbeda10f5fd0b80d
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-05-31 21:49:49 -0700

    Added file level documentation
    This adds file level documentation.  My main reason for doing this right
    now is if I try to create database tables, but don't select any, I get a
    `notice` and `warning`, and I want to fix how that ends up getting
    handled.  That *might* not end up being handled here, though I'm not
    sure.

--- view/admin/createsubmit.html.php
+++ view/admin/createsubmit.html.php
@@ -1,4 +1,19 @@
 <?php 
+/**
+ * View for results of creating database
+ *
+ * Database table creation.  Each table will be handled separately.
+ *
+ * @copyright 2016-2017 Datse Multimedia Productions
+ * @license GPL
+ * @license https://opensource.org/licenses/GPL-3.0 GNU Public License, version 3
+ * @author Jigme Datse Yli-Rasku <jigme.datse@datemultimedia.com>
+ * 
+ * @todo Have better handling if this gets called with no `$output`
+ *
+ * @since 0.0.0 
+ * @version 0.0.0 
+ */
 
 $title="Autism Jobs - Admin - Create Database Results";
 $pageHead="Autism Jobs Admin Page - Create Database Results";
