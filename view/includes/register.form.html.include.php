a0b2c745d2c86a4716583e5424382c91aefefb55
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-06-02 19:40:47 -0700

    Removed : from form labels
    This removes the : from the form labels.  This was used largely as a
    display thing, it has been replaced with CSS.

--- view/includes/register.form.html.include.php
+++ view/includes/register.form.html.include.php
@@ -1,11 +1,11 @@
 <form action="/index.php" method="post">
-	<label for="userame">Username</label>:
+	<label for="userame">Username</label>
 	<input type="text" name="username" id="username"><br />
-	<label for="email">Email</label>:
+	<label for="email">Email</label>
 	<input type="text" name="email" id="email"><br />
-	<label for="password">Password</label>:
+	<label for="password">Password</label>
 	<input type="password" name="password" id="password"><br />
-	<label for="passwordConfirm">Confirm Password</label>:
+	<label for="passwordConfirm">Confirm Password</label>
 	<input type="password" name="passwordConfirm" id="passwordConfirm"><br />
 	<input type="submit" value="preview"><input type="reset">
 </table> 
