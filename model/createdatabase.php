76c6e8d3633d0613edfe36ed2a175a3cc180323a
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-06-15 21:32:35 -0700

    Added PHP Docblock.
    I need to look at how to better handle this, but I am writing this for
    now as it is because the docblock needs to be added.
    
    There is something wrong with what I'm doing with creating the
    databases, and it is only ending up running (from what I can see) one
    (the latest) of the table creations.

--- model/createdatabase.php
+++ model/createdatabase.php
@@ -24,8 +24,22 @@
 $tables=cleanPostArray("tables");
 
 
-
-
+/**
+ * Create the tables
+ *
+ * This creates the tables.  
+ *
+ * @copyright 2016-2017 Datse Multimedia Productions
+ * @license GPL
+ * @license https://opensource.org/licenses/GPL-3.0 GNU Public License, version 3
+ * @author Jigme Datse Yli-Rasku <jigme.datse@datemultimedia.com>
+ * 
+ * @todo Find out why this is only doing one table at a time.
+ * @todo convert this to a function.
+ *
+ * @since 0.0.0 
+ * @version 0.0.0 
+ */
 foreach ($tables as $table) {
 	switch ($table) {
 		case "users":
