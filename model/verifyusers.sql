fde85bfb54a29dde111004690ee73437814406dd
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-06-15 21:28:11 -0700

    Fixed foreign key
    This has fixed the foreign key, and seems to work.

--- model/verifyusers.sql
+++ model/verifyusers.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS verifyusers (
 	verifyuserid		uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	userid		uuid FOREIGN KEY (userid) REFERENCES users (userid) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	userid		uuid REFERENCES users (userid) ON UPDATE NO ACTION ON DELETE NO ACTION,
 	checkhash	text NOT NULL,
 	created		TIMESTAMP NOT NULL DEFAULT now(),
 	verified	TIMESTAMP
