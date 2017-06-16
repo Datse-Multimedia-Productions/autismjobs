e6cc1e45a3a2acb05b6f0f536fa8c0f70bf0936c
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-06-15 23:10:04 -0700

    Start of registration functions

--- model/register.php
+++ model/register.php
@@ -18,3 +18,30 @@
  * @version 0.0.0 
  */
 
+$username=cleanPost("username");
+$email=cleanPost("email")
+$password=cleanPost("password");
+$passwordConfirm=cleanPost("passwordConfirm");
+
+/**
+ * Register User 
+ *
+ * This registers the user, it takes the arguments of username, email,
+ * password, and passwordConfrim.
+ *
+ * @copyright 2016-2017 Datse Multimedia Productions
+ * @license GPL
+ * @license https://opensource.org/licenses/GPL-3.0 GNU Public License, version 3
+ * @author Jigme Datse Yli-Rasku <jigme.datse@datemultimedia.com>
+ * 
+ * @todo Check data is sufficiently valid
+ * @todo Insert user into database
+ *
+ * @since 0.0.0 
+ * @version 0.0.0 
+ */
+function registerUser($username, $email, $password, $passwordConfirm) {
+
+}
+
+?>
