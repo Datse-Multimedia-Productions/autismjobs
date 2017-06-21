506cfb004b0a2aa77fc5090d815cf785726e2840
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-06-20 17:22:56 -0700

    Added string and type to verify table
    Updated to add the string and the type to the verify table.  This will
    allow for verification of different values for a given user.

--- model/verifyusers.sql
+++ model/verifyusers.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS verifyusers (
 	verifyuserid		uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	userid		uuid REFERENCES users (userid) ON UPDATE NO ACTION ON DELETE NO ACTION,
 	checkhash	text NOT NULL,
+	verifystring	text NOT NULL,
+	verifytype	text NOT NULL,
 	created		TIMESTAMP NOT NULL DEFAULT now(),
 	verified	TIMESTAMP
 );
