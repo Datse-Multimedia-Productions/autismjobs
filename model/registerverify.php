ca4490a9a95cadef4e0670127e7d5b2aa44cd787
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2017-09-29 14:11:25 -0700

    Basic data verification
    Just checking what we do for data verification.  The docblock needs to
    be updated, and we will sometime need to get better data verification
    for all of these values.

--- model/registerverify.php
+++ model/registerverify.php
@@ -38,9 +38,38 @@
 $action=cleanGet("action");
 $email=cleanGet("email");
 $user=cleanGet("user");
-$hash=cleanGet("hash);
+$hash=cleanGet("hash");
 
+/**
+* Handle the email verification
+*
+* This is the long description for the Class
+*
+* @param string $action Action from the form
+* @param string $email Email address we are verifying
+* @param string $user MD5 hashed value of userid
+* @param string $hash Hash value which we are using for verification
+* @return	mixed	Output results 
+* @see		??
+* @copyright	2017 Datse Multimedia Productions
+* @license	GPL
+* @license https://opensource.org/licenses/GPL-3.0 GNU Public License, version 3
+* @author Jigme Datse Yli-Rasku <jigme.datse@datemultimedia.com>
+* @author Datse Multimedia Productions <info@datsemultimedia.com>
+* 
+* @todo See where these values are coming from
+* @todo Look at how we are handling data connections
+* @todo Look at updating database
+* 
+* @since 0.0.0
+* @version 0.0.0
+*/
+function verifyEmail($action, $email, $user, $hash) {
+	if ($action=="register_verify" && !empty($email) && !empty($user) && !empty($hash) {
+		$output["status"]["data verification"]="Data verifies (we can use it)";
 
-
-
+	} else {
+		$output["status"]["data verification"]="Data Verification Failed";
+	}
+}
 ?>
